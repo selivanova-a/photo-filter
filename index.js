03905e2111af96eac2813423299d8f08d5564401
Anastasiia Selivanova <selivanovaana5tacia@yandex.ru>
2021-04-25 00:45:48 +0500

    feat: add canvas

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const inputs = document.querySelectorAll('input[type=range]');
 const outputs = document.querySelectorAll('.filters output');
-const buttonReset = document.querySelectorAll('.btn-reset');
-const fullscreenButton = document.querySelector('.fullscreen');
+const imageContainer = document.querySelector('.image-container');
+
+const buttonReset = document.getElementById('btn-reset');
+const buttonSave = document.getElementById('btn-save');
+const buttonNext = document.getElementById('btn-next');
+const buttonFullscreen = document.querySelector('.fullscreen');
+
 
 //CSS FILTERS ADD
 function addFilters() {
@@ -17,9 +22,22 @@ inputs.forEach(input => input.addEventListener('change', addFilters));
 inputs.forEach(input => input.addEventListener('mousemove', addFilters));
 inputs.forEach(input => input.addEventListener('input', scrollNumberUpdate));
 
+//RESET BUTTON DELETE FILTRES
+buttonReset.addEventListener('click', resetFiltres);
+
+function resetFiltres() {
+	outputs.forEach(output => {
+		output.innerHTML = '0';
+	});
+	inputs.forEach(input => {
+		input.value = input.defaultValue;
+		document.documentElement.style = '';
+	});
+}
+
 
 //FULLSCREEN
-fullscreenButton.addEventListener('click', toggleScreeen);
+buttonFullscreen.addEventListener('click', toggleScreeen);
 
 function toggleScreeen() {
 	if (!document.fullscreenElement) {
@@ -31,7 +49,6 @@ function toggleScreeen() {
 
 //LOAD IMAGE FROM COMPUTER
 const fileInput = document.querySelector('input[type="file"]');
-const imageContainer = document.querySelector('.image-container');
 
 fileInput.addEventListener('change', function(e) {
 	const file = fileInput.files[0];
@@ -45,19 +62,79 @@ fileInput.addEventListener('change', function(e) {
 	reader.readAsDataURL(file);
 });
 
-//IMAGE DAY AND NIGHT
+
+//CANVAS SAVE IMAGE
+
+//Draw image canvas
+const canvas = document.querySelector('canvas');
+
+function drawImage() {
+	const img = new Image();
+	img.setAttribute('crossOrigin', 'anonymous');
+	img.src = "assets/img/cat.png";
+
+	img.onload = function() {
+		canvas.width = img.width;
+		canvas.height = img.height;
+		const ctx = canvas.getContext("2d");
+		ctx.drawImage(img, 0, 0);
+	};
+}
+drawImage();
+
+//Save image canvas (not work)/////////////////////////////////////////////////////////////////////////////////////
+buttonSave.addEventListener('click', function(e) {
+	console.log(canvas.toDataURL());
+	let link = document.createElement('a');
+	link.download = 'download.png';
+	link.href = canvas.toDataURL();
+	link.click();
+	link.delete;
+});
+//Save with Filters (not work)///////////////////////////////////////////////////////////////////////////////////////
+
+//SLIDER IMAGE DAY or NIGHT
+let today = new Date(),
+	hour = today.getHours();
+
+let timeOfDay;
+let morning = 'morning';
+let day = 'day';
+let evening = 'evening';
+let night = 'night';
+
+if (hour >=6 && hour < 12) {
+	timeOfDay = morning;
+} else if (hour >=12 && hour < 18) {
+	timeOfDay = day;
+} else if (hour >= 18 && hour < 24) {
+	timeOfDay = evening;
+}
+else {
+	timeOfDay = night;
+}
+
+
+const base = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/`;
+const images = ['01.jpg', '02.jpg', '03.jpg', '05.jpg', '06.jpg', '07.jpg', '08.jpg', '09.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg'];
+let i = 0;
 
 
 
+function viewBgImage(src) {
+	const img = new Image();
+	img.src = src;
+	img.onload = () => {
+		imageContainer.innerHTML = "";
+		imageContainer.append(img);
+	};
+}
 
-
-
-
-
-
-
-
-
-
-
+function getImage() {
+	const index = i % images.length;
+	const imageSrc = base + images[index];
+	viewBgImage(imageSrc);
+	i++;
+}
+buttonNext.addEventListener('click', getImage);
 
